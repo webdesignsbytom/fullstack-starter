03d08952787cb7abd22155469da309bbac173c0b
xtombrock <xtombrock1989@gmail.com>
2023-01-22 21:30:49 +0000

    form setup

---
+++ vanilla/src/js/forms.js
@@ -0,0 +1,7 @@
+const resultsContainer = document.getElementById('form__results__container')
+const nameVar = document.getElementById('name')
+const emailVar = document.getElementById('email')
+const passwordVar = document.getElementById('password')
+const colourVar = document.getElementById('colour')
+
+nameVar.innerHTML += 'Tom'
