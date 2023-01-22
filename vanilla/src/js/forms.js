f62cf1535acf09040110b96cdcf9ae3cc20bbee8
xtombrock <xtombrock1989@gmail.com>
2023-01-22 21:50:49 +0000

    js linked

--- vanilla/src/js/forms.js
+++ vanilla/src/js/forms.js
@@ -1,7 +1,22 @@
 const resultsContainer = document.getElementById('form__results__container')
+const nameIn = document.getElementById('nameIn')
 const nameVar = document.getElementById('name')
+const emailIn = document.getElementById('emailIn')
 const emailVar = document.getElementById('email')
+const passwordIn = document.getElementById('passwordIn')
 const passwordVar = document.getElementById('password')
+const colourIn = document.getElementById('colourIn')
 const colourVar = document.getElementById('colour')
 
-nameVar.innerHTML += 'Tom'
+nameIn.addEventListener('input', (event) => {
+    nameVar.innerText = `Name: ` + event.target.value
+})
+emailIn.addEventListener('input', (event) => {
+    emailVar.innerText = `Email: ` + event.target.value
+})
+passwordIn.addEventListener('input', (event) => {
+    passwordVar.innerText = `Password: ` + event.target.value
+})
+colourIn.addEventListener('input', (event) => {
+    colourVar.innerText = `Colour: ` + event.target.value
+})
