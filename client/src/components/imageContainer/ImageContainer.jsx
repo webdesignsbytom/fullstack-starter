2afb426995ffec67ee2158c477e18084e66dc30e
xtombrock <xtombrock1989@gmail.com>
2023-01-29 23:16:42 +0000

    height adjusting framge

--- client/src/components/imageContainer/ImageContainer.jsx
+++ client/src/components/imageContainer/ImageContainer.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from 'react'
 import './imageContainer.css'
+import { sliderOptions } from '../../utils/ImageLocations';
+
 function ImageContainer() {
-    const [imageArray, setImageArray] = useState([])
+  const displayImagesArray = sliderOptions;
+
+    const [imageArray, setImageArray] = useState(displayImagesArray)
+    const [imageIndex, setImageIndex] = useState(0)
+
+    const nextImage = () => {
+      setImageIndex(prev => prev + 1)
+    }
 
   return (
+    <>
     <div className='imageContainer__container'>
         <div className="main__container">
-            image
+            <div className="image__container">
+              <img src={displayImagesArray[imageIndex]} alt="Array" />
+            </div>
         </div>
+    <button onClick={nextImage}>NEXT</button>
     </div>
+    </>
   )
 }
 
