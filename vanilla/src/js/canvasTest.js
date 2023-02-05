53c09b2f11040c15e1df8f58056379b57add2763
xtombrock <xtombrock1989@gmail.com>
2023-02-05 22:21:50 +0000

    save cavas smiley face

---
+++ vanilla/src/js/canvasTest.js
@@ -0,0 +1,49 @@
+const canvas = document.getElementById('canvas');
+
+let context = canvas.getContext('2d');
+
+class Algae {
+  constructor(xpos, ypos, radius, color) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw(context) {
+    context.beginPath();
+
+    context.strokeStyle = this.color;
+    context.lineWidth = 5;
+
+    context.arc(this.xpos, this.ypos, this.radius, Math.PI * 2, false);
+
+    context.stroke();
+    context.closePath();
+  }
+}
+
+function createAlgae() {
+    console.log('creating')
+    const newAlgae = new Algae(100, 100, 5, 'red')
+    console.log('newAlgae', newAlgae)
+    newAlgae.draw(context)
+}
+
+function draw() {
+    const canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+  
+      ctx.beginPath();
+      ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+      ctx.moveTo(110, 75);
+      ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
+      ctx.moveTo(65, 65);
+      ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
+      ctx.moveTo(95, 65);
+      ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
+      ctx.stroke();
+    }
+  }
+  
