bb2d2c081b1ea9d456d9f98d2c4e666c850dce62
Thomas Brockington <xtombrock1989@gmail.com>
2023-02-11 18:04:03 +0000

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,21 @@ import RegistrationPage from './users/register/Register';
 import LoginPage from './users/login/Login';
 import Form from './components/forms/forms2/Form';
 import ImageContainer from './components/imageContainer/ImageContainer';
+import Error404 from './pages/error/Error404';
+import Home from './pages/home/Home404';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
-      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegistrationPage />} />
+
+      <Route path="/Home" element={<Home />} />
+      
       <Route path="/forms" element={<Form />} />
       <Route path="/imageContainer" element={<ImageContainer />} />
+     
+      <Route path="*" element={<Error404 />} /> */ All link enteries that are not registered lead to 404 page /*
     </Routes>
   );
 }
--- vanilla/README.md
+++ vanilla/README.md
@@ -1,4 +1,11 @@
+## Pages Contains
+Forms
+AutoTyping text
+Canvas with moving objects
+CSS shapes - phone/laptop
+
 ## Animations include
+
 - Light Speed
 - Bouncing Tennis Ball
 - Glowing letters of a word cylcing through
