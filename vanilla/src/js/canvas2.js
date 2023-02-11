41e48ffd1737d27fa1a5815ddb1b495887daf59f
xtombrock <xtombrock1989@gmail.com>
2023-02-11 10:18:13 +0000

    deleteable

--- vanilla/src/js/canvas2.js
+++ vanilla/src/js/canvas2.js
@@ -1,11 +1,11 @@
 const canvas = document.getElementById('canvas');
-const countContainer = document.getElementById('click__count')
-const testContainer = document.getElementById('test__count')
+const countContainer = document.getElementById('click__count');
+const testContainer = document.getElementById('test__count');
 
 canvas.addEventListener('click', (e) => {
   var x = e.pageX - canvas.offsetLeft;
   var y = e.pageY - canvas.offsetTop;
-  console.log('x', x)
+  console.log('x', x);
   console.log('y', y);
 
   algaeArray.forEach(function (algae) {
@@ -27,16 +27,18 @@ canvas.width = window_width;
 canvas.height = window_height;
 
 class Algae {
-  constructor(xpos, ypos, radius, color, count, speed) {
+  constructor(id, xpos, ypos, radius, color, count, speed) {
+    this.id = id;
     this.xpos = xpos;
     this.ypos = ypos;
     this.radius = radius;
     this.color = color;
     this.count = count;
-    this.clicked = function() {
-        console.log('CLIIIIIIICKED');
-        clickedOnCount++;
-        renderCounts();
+    this.clicked = function () {
+      console.log('CLIIIIIIICKED', this);
+      clickedOnCount++;
+      renderCounts();
+      this.delete()
     };
     this.speed = speed;
 
@@ -62,35 +64,41 @@ class Algae {
   }
 
   update() {
-    context.clearRect(0, 0, canvas.width, canvas.height)
-
     this.draw(context);
 
     if (this.xpos > canvas.width) {
-        this.dx = -this.dx;
+      this.dx = -this.dx;
     }
 
-    if ( (this.xpos - this.radius) < 0) {
-        this.dx = -this.dx;
+    if (this.xpos - this.radius < 0) {
+      this.dx = -this.dx;
     }
 
     if (this.ypos > canvas.height) {
-        this.dy = -this.dy;
+      this.dy = -this.dy;
     }
 
-    if ( (this.ypos - this.radius) < 0) {
-        this.dy = -this.dy;
+    if (this.ypos - this.radius < 0) {
+      this.dy = -this.dy;
     }
 
     this.xpos += this.dx;
     this.ypos += this.dy;
+  }
+
+  delete() {
+    console.log('DELETE DELETE')
+    const foundAlgae = algaeArray.find((a) => a.id === this.id)
+    const algaeIndex = algaeArray.indexOf(foundAlgae)
+    console.log('foundAlgae', algaeIndex)
+    algaeArray.splice(algaeIndex, 1)
   }
 }
 
 let createArray = 10;
 let algaeCounter = 1;
 let algaeArray = [];
-let clickedOnCount = 0
+let clickedOnCount = 0;
 
 // On click event swarm
 function clickFunctionSwarm() {
@@ -98,7 +106,15 @@ function clickFunctionSwarm() {
     let random_x = Math.random() * window_width;
     let random_y = Math.random() * window_height;
 
-    const algaeSwarm = new Algae(random_x, random_y, 20, 'red', algaeCounter, 2);
+    const algaeSwarm = new Algae(
+      i,
+      random_x,
+      random_y,
+      20,
+      'red',
+      algaeCounter,
+      1
+    );
     algaeSwarm.draw(context);
     algaeArray.push(algaeSwarm);
 
@@ -107,19 +123,20 @@ function clickFunctionSwarm() {
 }
 
 function updateSwarm() {
-    requestAnimationFrame(updateSwarm);
-    algaeArray.forEach((algaeSwarm => {
-        algaeSwarm.update()
-    })) 
+  requestAnimationFrame(updateSwarm);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  algaeArray.forEach((algaeSwarm) => {
+    algaeSwarm.update();
+  });
 }
 
 function renderCounts() {
-    countContainer.innerText = `Algae Clicked On: ${clickedOnCount}`
+  countContainer.innerText = `Algae Clicked On: ${clickedOnCount}`;
 }
 
 function run() {
-    renderCounts();
-    updateSwarm()
+  renderCounts();
+  updateSwarm();
 }
 
 run();
