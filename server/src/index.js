8194c1b35e2095ea9198f370dace5b1df6887e3e
Thomas Brockington <xtombrock1989@gmail.com>
2023-02-21 01:16:00 +0000

    utils

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,13 @@
 // Load our .env file
 require('dotenv').config();
+
 // Load app
 const app = require('./server.js')
-// Set the port
+
+// Set the port and URl
 const PORT = process.env.PORT || 4000;
-const URL = process.env.URL || 'http://localhost:'
+const URL = process.env.HTTP_URL || 'http://localhost:'
+
 // Start our API server
 app.listen(PORT, () => {
     console.log(`\nServer is running on ${URL}${PORT}\n`);
