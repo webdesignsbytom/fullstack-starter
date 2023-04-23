3e7524c37b6fc901f146db73253007e5a7a10598
xtombrock <xtombrock1989@gmail.com>
2023-04-23 17:43:12 +0100

    save

--- client/src/context/ToggleContext.jsx
+++ client/src/context/ToggleContext.jsx
@@ -5,27 +5,12 @@ export const ToggleContext = React.createContext();
 
 const ToggleContextProvider = ({ children }) => {
   const [toggleNavigation, setToggleNavigation] = useState(false);
-  const [toggleNotifications, setToggleNotifications] = useState(false);
-  const [toggleMessages, setToggleMessages] = useState(false);
-  const [toggleEvents, setToggleEvents] = useState(false);
-  const [toggleContacts, setToggleContacts] = useState(false);
-  const [toggleTests, setToggleTests] = useState(false);
 
   return (
     <ToggleContext.Provider
       value={{
         toggleNavigation,
         setToggleNavigation,
-        toggleNotifications,
-        setToggleNotifications,
-        toggleTests,
-        setToggleTests,
-        toggleMessages,
-        setToggleMessages,
-        toggleEvents,
-        setToggleEvents,
-        toggleContacts,
-        setToggleContacts,
       }}
     >
       {children}
