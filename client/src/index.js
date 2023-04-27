50e89cb853f8c74180d760da0923ddc995502361
Thomas Brockington <xtombrock1989@gmail.com>
2023-04-27 16:09:14 +0100

    save

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+// Context
 import UserContextProvider from './context/UserContext';
+// Styles
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
