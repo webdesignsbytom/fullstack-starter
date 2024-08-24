7fc226b8da5855aea6b6371b8cdd5a547282a149
xtombrock <xtombrock1989@gmail.com>
2024-08-25 00:30:32 +0100

    save

--- server/src/utils/config.js
+++
@@ -1,9 +0,0 @@
-// Saves typing. Import from config.js removes proc.env
-// Use on pages to call
-export const JWT_SECRET = process.env.JWT_SECRET
-export const JWT_EXPIRY = process.env.JWT_EXPIRY
-export const HTTP_URL = process.env.HTTP_URL
-export const VERIFICATION_URL = process.env.VERIFICATION_URL
-export const PORT = process.env.PORT
-export const AUTH_EMAIL = process.env.AUTH_EMAIL
-export const RESET_EMAIL = process.env.RESET_EMAIL
