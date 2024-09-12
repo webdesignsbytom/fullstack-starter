a8635df0055286186218c7ac676fe0ee6d1cf44f
Thomas Brockington <xtombrock1989@gmail.com>
2024-09-12 23:14:08 +0100

    savcve

--- vanilla/sideScroller/scripts/main.js
+++ vanilla/sideScroller/scripts/main.js
@@ -67,7 +67,7 @@ window.addEventListener('load', function () {
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
 
-  canvas.width = 720;
+  canvas.width = 1720;
   canvas.height = 720;
 
   const game = new Game(canvas, ctx);
